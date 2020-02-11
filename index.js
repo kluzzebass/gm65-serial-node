297aadad84652ff05a6426db3df6b57b91529d68
Jan Fredrik Leversund <kluzz@radical.org>
2020-02-11 08:27:33 +0000

    just more code

--- index.js
+++ index.js
@@ -16,14 +16,19 @@ const bitRates = [
 const defaults = {
     bitRate: 9600
 }
-  
+
+const commands = {
+  trigger: Buffer.from([0x7e, 0x00, 0x08, 0x01, 0x00, 0x02, 0x01, 0xab, 0xcd]),
+  confirmation: Buffer.from([0x02, 0x00, 0x00, 0x01, 0x00, 0x33, 0x31])
+}
+
 
 module.exports = (dev, options = {}) => {
     this.dev = dev
     this.buf = Buffer.alloc(2048) // Probably enough...?
     this.port = null
 
-    this.bitRate = options.bitRate || defaults.bitRate
+    this.bitRate = (options.bitRate && bitRates.includes(options.bitRate)) || defaults.bitRate
 
     this.port = new SerialPort(this.dev, {
         baudRate: this.bitRate,
@@ -42,4 +47,10 @@ module.exports = (dev, options = {}) => {
     this.port.on('data', data => {
         console.log(data)
     })
+
+    this.trigger = () => {
+      this.port.write(commands.trigger)
+    }
+
+    return this
 }
--- test/index.js
+++ test/index.js
@@ -2,3 +2,5 @@ const reader = require('../index.js')('/dev/ttyACM0', {
     // baudRate: 115200
 })
 
+reader.trigger()
+
